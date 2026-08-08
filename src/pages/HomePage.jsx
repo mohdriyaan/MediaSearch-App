cfb9d98a6eb4f157b8dc3912b43dcc6a9c21ebff
Mohammed Riyaan <mohammedrayaan1@gmail.com>
2026-08-08 20:43:15 +0530

    feat: improve search results context

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,25 +3,24 @@ import ResultGrid from "../components/ResultGrid"
 import SearchBar from "../components/SearchBar"
 import Tabs from "../components/Tabs"
 
-
 const HomePage = () => {
-  const query = useSelector((state)=>state.search.query)
+  const { query, results } = useSelector((state) => state.search)
 
   return (
-    <div>
-      
+    <main>
       <SearchBar />
-      {query !== "" 
-      ? 
-        <div>
+      {query && (
+        <section>
           <Tabs />
+          <div className="mx-auto flex max-w-5xl items-center justify-between px-4 pb-5">
+            <p className="text-sm text-slate-400">Results for <span className="font-semibold text-slate-200">“{query}”</span></p>
+            <p className="text-xs text-slate-500">{results.length} loaded</p>
+          </div>
           <ResultGrid />
-        </div>  
-      :
-        null      
-      }
-      
-    </div>
+        </section>
+      )}
+    </main>
   )
 }
+
 export default HomePage
